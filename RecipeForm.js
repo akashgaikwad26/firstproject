b85c6201d314ce0540bc42476b32e5475a38cf36
AkashPravi1 <159224590+AkashPravi1@users.noreply.github.com>
2024-07-16 14:49:16 +0530

    Create RecipeForm.js

---
+++ RecipeForm.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+export const RecipeForm = ({ addRecipe }) => {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addRecipe({ title, description });
+    setTitle('');
+    setDescription('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Add Recipe</h2>
+      <input
+        type="text"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        placeholder="Title"
+        required
+      />
+      <textarea
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+        placeholder="Description"
+        required
+      />
+      <button type="submit">Add Recipe</button>
+    </form>
+  );
+};
